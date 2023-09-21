1eafa20b2b622fd5cc8e9dc0fd6ec94f70c1a0cf
zainaboyedeji <zainaboyedeji95@gmail.com>
2023-09-21 10:59:55 +0100

    lendsqr-backend-initial-commit

---
+++ src/common/middlewares/auth.validation.middleware.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken'),
+secret = require('../config/env.config')['jwt_secret'];
+const ADMIN_PERMISSION = require('../config/env.config')['permissionLevels']['ADMIN'];
+
+exports.minimumPermissionLevelRequired = (required_permission_level) => {
+    return (req, res, next) => {
+        let user_permission_level = parseInt(req.jwt.permissionLevel);
+        let userId = req.jwt.userId;
+        if (user_permission_level & required_permission_level) {
+            return next();
+        } else {
+            return res.status(403).send();
+        }
+    };
+};
+
+exports.onlySameUserOrAdminCanDoThisAction = (req, res, next) => {
+    let user_permission_level = parseInt(req.jwt.permissionLevel);
+    let userId = req.jwt.userId;
+    if (req.params && req.params.userId && userId === req.params.userId) {
+        return next();
+    } else {
+        if (user_permission_level & ADMIN_PERMISSION) {
+            return next();
+        } else {
+            return res.status(403).send();
+        }
+    }
+
+};
+
+exports.sameUserCantDoThisAction = (req, res, next) => {
+    let userId = req.jwt.userId;
+    if (req.params.userId !== userId) {
+        return next();
+    } else {
+        return res.status(400).send();
+    }
+};
+
+exports.validJWTNeeded = (req, res, next) => {
+    if (req.headers['authorization']) {
+        try {
+            let authorization = req.headers['authorization'].split(' ');
+            if (authorization[0] !== 'Bearer') {
+                return res.status(401).send();
+            } else {
+                req.jwt = jwt.verify(authorization[1], secret);
+                return next();
+            }
+        } catch (err) {
+            return res.status(403).send();
+        }
+    } else {
+        return res.status(401).send();
+    }
+}; 
